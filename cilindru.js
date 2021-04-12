017c8f6212e77f2ba20312b586e7fdea67547c09
tirivoi <74932172+tirivoi@users.noreply.github.com>
2021-04-12 18:43:26 +0300

    Add files via upload

---
+++ cilindru.js
@@ -0,0 +1,6 @@
+var r = prompt("Please enter number r:");
+var h = prompt("Please enter number h:");
+var π = 3.14;
+
+var volum = π * parseInt(r) * parseInt(r) * parseInt(h);
+alert("Sum is: " + volum);
